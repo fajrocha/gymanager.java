8100d4df3bf74f2d6a294d88b7712f2b20899be2
fajrocha_apc <fabio.jr@ua.pt>
2024-09-07 22:51:37 +0100

    Finished the documentation of the User Management bounded context.

--- gymanager/src/main/java/com/faroc/gymanager/usermanagement/api/users/controllers/v1/UsersController.java
+++ gymanager/src/main/java/com/faroc/gymanager/usermanagement/api/users/controllers/v1/UsersController.java
@@ -1,14 +1,22 @@
 package com.faroc.gymanager.usermanagement.api.users.controllers.v1;
 
 import an.awesome.pipelinr.Pipeline;
+import com.faroc.gymanager.common.api.contracts.responses.ValidationProblemDetail;
 import com.faroc.gymanager.usermanagement.application.admins.commands.addadmin.AddAdminCommand;
 import com.faroc.gymanager.usermanagement.application.participants.commands.addparticpant.AddParticipantCommand;
 import com.faroc.gymanager.usermanagement.application.trainers.commands.addtrainer.AddTrainerCommand;
 import com.faroc.gymanager.usermanagement.api.users.contracts.v1.responses.AdminCreatedResponse;
 import com.faroc.gymanager.usermanagement.api.users.contracts.v1.responses.ParticipantCreatedResponse;
 import com.faroc.gymanager.usermanagement.api.users.contracts.v1.responses.TrainerCreatedResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -25,7 +33,55 @@ public class UsersController {
     }
 
     @PostMapping("/admin")
-    public AdminCreatedResponse createAdminProfile(@PathVariable UUID userId) {
+    @Operation(
+            summary = "Add admin profile to user.",
+            description = "Adds an admin profile to user which requests it, giving him admin privileges."
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Profile added response, which includes the admin id.",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = AdminCreatedResponse.class)
+                            )
+                    }),
+            @ApiResponse(responseCode = "400",
+                    description = "Invalid login data on the request body.",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ValidationProblemDetail.class)
+                            )
+                    }),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "User on the request token is not the same as the user requesting to add the profile",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Resource not found, for example when the user does not exist.",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ProblemDetail.class)
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal server error during adding profile request.",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ProblemDetail.class)
+                            )
+                    })
+    })
+    public AdminCreatedResponse createAdminProfile(
+            @Parameter(description = "User id to add admin profile") @PathVariable UUID userId) {
         var command = new AddAdminCommand(userId);
 
         var adminId = command.execute(pipeline);
@@ -34,7 +90,55 @@ public class UsersController {
     }
 
     @PostMapping("/trainer")
-    public TrainerCreatedResponse createTrainerProfile(@PathVariable UUID userId) {
+    @Operation(
+            summary = "Add trainer profile to user.",
+            description = "Adds a trainer profile to user which requests it, giving him trainer privileges."
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Profile added response, which includes the trainer id.",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = TrainerCreatedResponse.class)
+                            )
+                    }),
+            @ApiResponse(responseCode = "400",
+                    description = "Invalid login data on the request body.",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ValidationProblemDetail.class)
+                            )
+                    }),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "User on the request token is not the same as the user requesting to add the profile",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Resource not found, for example when the user does not exist.",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ProblemDetail.class)
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal server error during adding profile request.",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ProblemDetail.class)
+                            )
+                    })
+    })
+    public TrainerCreatedResponse createTrainerProfile(
+            @Parameter(description = "User id to add trainer profile") @PathVariable UUID userId) {
         var command = new AddTrainerCommand(userId);
 
         var trainerId = command.execute(pipeline);
@@ -43,7 +147,55 @@ public class UsersController {
     }
 
     @PostMapping("/participant")
-    public ParticipantCreatedResponse createParticipantProfile(@PathVariable UUID userId) {
+    @Operation(
+            summary = "Add participant profile to user.",
+            description = "Adds a participant profile to user which requests it, giving him participant privileges."
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Profile added response, which includes the participant id.",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ParticipantCreatedResponse.class)
+                            )
+                    }),
+            @ApiResponse(responseCode = "400",
+                    description = "Invalid login data on the request body.",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ValidationProblemDetail.class)
+                            )
+                    }),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "User on the request token is not the same as the user requesting to add the profile",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Resource not found, for example when the user does not exist.",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ProblemDetail.class)
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal server error during adding profile request.",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ProblemDetail.class)
+                            )
+                    })
+    })
+    public ParticipantCreatedResponse createParticipantProfile(
+            @Parameter(description = "User id to add participant profile") @PathVariable UUID userId) {
         var command = new AddParticipantCommand(userId);
 
         var participantId = command.execute(pipeline);
